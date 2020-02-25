fb50f9309352a07c36419c586cc98f44bdba7947
mtparker <mtparker@dundee.ac.uk>
2020-02-25 11:02:35 +0000

    improved documentation for command line interface

--- lib2pass/main.py
+++ lib2pass/main.py
@@ -1,3 +1,6 @@
+'''
+lib2pass.main: contains the command line interface for 2passtools
+'''
 import os
 import logging
 
@@ -23,6 +26,12 @@ def _all_predictions(introns, motifs, counts, jad_labels,
                      lr_low_confidence_threshold,
                      lr_high_confidence_threshold,
                      processes):
+    '''
+    Takes as input the alignment metrics extracted either from a bam file (2passtools score)
+    or previously created junction bed file (2passtools merge). Calculates decision tree
+    score one, then extracts junction sequences from the fasta file and calculates
+    decision tree score two.
+    '''
     log.info(f'Identified {len(introns):d} introns')
     dt1_labels = dt1_pred(
         motifs, jad_labels, is_primary_donor, is_primary_acceptor,
@@ -66,27 +75,58 @@ def main():
     pass
 
 
+SCORE_MERGE_COMMON_OPTIONS = [
+    click.option('-o', '--output-bed-fn', required=True, help='Output file path'),
+    click.option('-f', '--ref-fasta-fn', required=True, type=str,
+                  help='Path to the fasta file that reads were mapped to'),
+    click.option('-j', '--jad-size-threshold', default=4, help='JAD to threshold at in the decision tree'),
+    click.option('-d', '--primary-splice-local-dist', default=20,
+                  help='Distance to search for alternative donor/acceptors when calculating primary d/a'),
+    click.option('-m', '--canonical-motifs', default='GTAG|GCAG|ATAG', callback=validate_motif_regex,
+                  help=('Intron motifs considered canonical in organism. '
+                        'Should be four char DNA motifs separated by vertical bar only')),
+    click.option('-w', '--lr-window-size', default=128, type=int,
+                  help='Sequence size to extract to train logistic regression models'),
+    click.option('-k', '--lr-kfold', default=6, type=int,
+                  help='Number of cross validation k-folds for logistic regression models'),
+    click.option('-lt', '--lr-low-confidence-threshold', default=0.1, type=float,
+                  help='Logistic regression low confidence threshold for decision tree 2'),
+    click.option('-ht', '--lr-high-confidence-threshold', default=0.6, type=float,
+                  help='Logistic regression high confidence threshold for decision tree 2'),
+    click.option('--stranded/--unstranded', default=True,
+                  help=('Whether input data is stranded or unstranded. '
+                        'direct RNA is stranded, cDNA often isn\'t')),
+    click.option('-p', '--processes', default=1),
+    click.option('-s', '--random-seed', default=None, type=int),
+    click_log.simple_verbosity_option(log),
+]
+
+def _common_options(common_options):
+    def _apply_common_options(func):
+        for option in reversed(common_options):
+            func = option(func)
+        return func
+    return _apply_common_options
+
+
 @main.command()
 @click.argument('bam-fn', required=True, nargs=1)
-@click.option('-o', '--output-bed-fn', required=True)
-@click.option('-f', '--ref-fasta-fn', required=True, type=str)
-@click.option('-j', '--jad-size-threshold', default=4)
-@click.option('-d', '--primary-splice-local-dist', default=20)
-@click.option('-m', '--canonical-motifs', default='GTAG|GCAG|ATAG', callback=validate_motif_regex)
-@click.option('-w', '--lr-window-size', default=128, type=int)
-@click.option('-k', '--lr-kfold', default=6, type=int)
-@click.option('-lt', '--lr-low-confidence-threshold', default=0.1, type=float)
-@click.option('-ht', '--lr-high-confidence-threshold', default=0.6, type=float)
-@click.option('--stranded/--unstranded', default=True)
-@click.option('-p', '--processes', default=1)
-@click.option('-s', '--random-seed', default=None, type=int)
-@click_log.simple_verbosity_option(log)
+@_common_options(SCORE_MERGE_COMMON_OPTIONS)
 def score(bam_fn, output_bed_fn, ref_fasta_fn,
           jad_size_threshold,
           primary_splice_local_dist, canonical_motifs,
           lr_window_size, lr_kfold,
           lr_low_confidence_threshold, lr_high_confidence_threshold,
           stranded, processes, random_seed):
+    '''
+    2passtools score: A tool for extracting and scores junctions from a bam file
+    aligned with minimap2. Filtered junctions can be used to realign reads in
+    a second pass with minimap2.
+
+    Bam files must be mapped with minimap2 and have the long form CS tag, e.g.
+
+    minimap2 -a --cs=long -k14 -x splice ref.fa reads.fq
+    '''
 
     if random_seed is not None:
         np.random.seed(random_seed)
@@ -121,25 +161,17 @@ def score(bam_fn, output_bed_fn, ref_fasta_fn,
 
 
 @main.command()
-@click_log.simple_verbosity_option(log)
-@click.argument('bed-fns', nargs=-1, required=True)
-@click.option('-o', '--output-bed-fn', required=True)
-@click.option('-f', '--ref-fasta-fn', required=True, type=str)
-@click.option('-j', '--jad-size-threshold', default=4)
-@click.option('-d', '--primary-splice-local-dist', default=20)
-@click.option('-m', '--canonical-motifs', default='GTAG|GCAG|ATAG', callback=validate_motif_regex)
-@click.option('-w', '--lr-window-size', default=128, type=int)
-@click.option('-k', '--lr-kfold', default=6, type=int)
-@click.option('-lt', '--lr-low-confidence-threshold', default=0.1, type=float)
-@click.option('-ht', '--lr-high-confidence-threshold', default=0.6, type=float)
-@click.option('-p', '--processes', default=1)
-@click.option('-s', '--random-seed', default=None, type=int)
+@_common_options(SCORE_MERGE_COMMON_OPTIONS)
 def merge(bed_fns, output_bed_fn, ref_fasta_fn,
           jad_size_threshold, primary_splice_local_dist, canonical_motifs,
           lr_window_size, lr_kfold,
           lr_low_confidence_threshold, lr_high_confidence_threshold,
           processes, random_seed):
-
+    '''
+    2passtools merge: Merges bed files produced by 2passtools score on individual
+    replicates and recalculates junction strength metrics. Can be used to create
+    a unified junction set to realign reads from different replicates.
+    '''
     if random_seed is not None:
         np.random.seed(random_seed)
 
@@ -175,6 +207,10 @@ def merge(bed_fns, output_bed_fn, ref_fasta_fn,
 @click.option('--exprs', required=False, default="decision_tree_2_pred")
 @click_log.simple_verbosity_option(log)
 def filter(bed_fn, output_bed_fn, exprs):
+    '''
+    2passtools filter: Convenience tool to filter a junction bed and produce
+    6-column bed format which is compatible with minimap2.
+    '''
     with open(output_bed_fn, 'w') as bed:
         for chrom, start, end, strand, decision in apply_eval_expression(bed_fn, exprs):
             if decision:
